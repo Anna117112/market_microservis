fec16bac0210a480a3ae6b00fa2b2f643f18f83d
Anna117112 <92026992+Anna117112@users.noreply.github.com>
2023-04-17 21:00:11 +0300

    Add files via upload

---
+++ spring_market_DZ_3_ASP/src/main/java/com/geekbrains/spring_data1/SpringData1Application.java
@@ -0,0 +1,31 @@
+package com.geekbrains.spring_data1;
+
+import com.geekbrains.spring_data1.config.AppConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+/*
+
+ */
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class SpringData1Application  {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(SpringData1Application.class, args);
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+
+
+		context.close();
+	}
+
+}
