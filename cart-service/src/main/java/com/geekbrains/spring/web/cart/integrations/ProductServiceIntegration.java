ebe316ba1062d2e74267d69886b55f57f9be337b
Anna <a.dyakonu85@mail.ru>
2023-04-17 22:33:00 +0300

    First

---
+++ cart-service/src/main/java/com/geekbrains/spring/web/cart/integrations/ProductServiceIntegration.java
@@ -0,0 +1,28 @@
+package com.geekbrains.spring.web.cart.integrations;
+
+
+import com.geekbrains.spring.web.api.carts.CartDto;
+import com.geekbrains.spring.web.api.core.ProductDto;
+import com.geekbrains.spring.web.api.exceptions.CartServiceAppError;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class ProductServiceIntegration {
+    private final WebClient productServiceWebClient;
+
+
+    public Optional<ProductDto> findById(Long id) {
+        ProductDto productDto = productServiceWebClient.get()
+                .uri("/api/v1/products/" + id)
+                .retrieve()
+                .bodyToMono(ProductDto.class)
+                .block();
+        return Optional.ofNullable(productDto);
+    }
+}
