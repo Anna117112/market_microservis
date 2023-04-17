59435486fb5439e46a0d7c9bba70421a98292c63
Anna117112 <92026992+Anna117112@users.noreply.github.com>
2023-04-17 21:04:51 +0300

    Add files via upload

---
+++ spring_market_microservic_DZ_5/front-service/src/main/java/com/flamexander/cloud/front/service/FrontApp.java
@@ -0,0 +1,44 @@
+package com.flamexander.cloud.front.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class FrontApp {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontApp.class, args);
+    }
+
+    @GetMapping("/front-service/api/v1/products")
+    public List<Object> getAllProducts()
+
+    {
+
+        return (List<Object>) restTemplate.getForObject("http://product-service/api/v1/products", Object.class);
+
+
+    }
+
+
+}
